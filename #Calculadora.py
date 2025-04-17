162a017cf320b270ee474728a1fa6dfde782c050
JulioPR <156125579+LegacyofJulio@users.noreply.github.com>
2025-04-17 15:13:11 -0400

    Add files via upload

---
+++ #Calculadora.py
@@ -0,0 +1,55 @@
+# Calculadora
+import math
+
+
+def principio_calculadora():
+    print("---------------Calculadora basica---------------")
+    print("""
+    Las operaciones disponibles son:
+         
+    1-Sumar
+    2-Restar
+    3-Dividir
+    4-Multiplicar \n""")
+
+    while True:
+        operacion = input("Cual deseas realizar: ")
+
+        try:
+            operacion = int(operacion)
+            if operacion in [1, 2, 3, 4]:
+                break
+
+        except ValueError:
+            print("Escribe un operador valido (1-4)")
+
+    while True:
+        primer_numero = input("\nPrimer numero: ")
+        segundo_numero = input("Segundo numero: ")
+
+        try:
+            primer_numero = float(primer_numero)
+            segundo_numero = float(segundo_numero)
+            break  # Exit the loop if conversion succeeds
+        except ValueError:
+            print("Insertar un numero valido")
+
+    if operacion == 1:
+        print(primer_numero + segundo_numero)
+    elif operacion == 2:
+        print(primer_numero - segundo_numero)
+    elif operacion == 3:
+        print(primer_numero / segundo_numero)
+    elif operacion == 4:
+        print(primer_numero * segundo_numero)
+
+
+principio_calculadora()
+
+volver_usar = input("\nQuieres volver a usarla denuevo? (Si/No) ")
+
+if volver_usar.lower == "si":
+    principio_calculadora()
+
+else:
+    print("\nGracias por usar la calculadora")
